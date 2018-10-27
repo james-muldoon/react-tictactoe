a05da5e4dc9322abdb9dc88bcfe6cbe3579098c5
James Muldoon <james.muldoon@outlook.com>
2018-10-27 22:50:29 +1100

    implementing time travel

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -53,12 +53,13 @@ class Game extends React.Component {
             history: [{
                 squares: Array(9).fill(null)
             }],
+            stepNumber: 0,
             xIsNext: true
         };
     }
 
     handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
 
@@ -71,25 +72,33 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: history.length,
             xIsNext: !this.state.xIsNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
         });
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current);
 
         const moves = history.map((step, move) => {
             const desc = move ?
-              'Go to move #' + move :
-              'Go to game start';
+                'Go to move #' + move :
+                'Go to game start';
             return (
-              <li>
-                <button onClick={() => this.jumpTo(move)}>{desc}</button>
-              </li>
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
             );
-          });
+        });
 
         let status;
         if (winner) {
@@ -102,7 +111,7 @@ class Game extends React.Component {
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board 
+                    <Board
                         squares={current.squares}
                         onClick={(i) => this.handleClick(i)}
                     />
